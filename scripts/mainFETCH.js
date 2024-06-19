508d23df87c6409c5a1ae1626cf6107949534680
Fred Menge <mengeprofissional@gmail.com>
2024-06-19 15:53:38 -0300

    Adição das funiconalidades de TRY - CATCH - FINALLY

--- scripts/mainFETCH.js
+++ scripts/mainFETCH.js
@@ -4,27 +4,34 @@ $(document).ready(function () {
         //AJAX - Asynchronous Javascript and XML 
 
         const cep = $('#cep').val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`
+        const endpoint = `https://viacep.com.br/ws/${cep}/json/`
         $(this).find('i').addClass('d-none');
         $(this).find('span').removeClass('d-none');
 
         fetch(endpoint).then(function (resposta) {
 
             return resposta.json()
+        }).then(function (json) {
+
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} -  ${estado}`;
+
+            $('#endereco').val(endereco);
+
+            $('#btn-buscar-cep').find('i').removeClass('d-none');
+            $('#btn-buscar-cep').find('span').addClass('d-none');
+
+        }).catch(function (erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde!")
+
+        }).finally(function () {
+
+            $('#btn-buscar-cep').find('i').removeClass('d-none');
+            $('#btn-buscar-cep').find('span').addClass('d-none');
         })
-            .then(function (json) {
-
-                const logradouro = json.logradouro;
-                const bairro = json.bairro;
-                const cidade = json.localidade;
-                const estado = json.uf;
-                const endereco = `${logradouro}, ${bairro} - ${cidade} -  ${estado}`;
-
-                $('#endereco').val(endereco);
-
-                $('#btn-buscar-cep').find('i').removeClass('d-none');
-                $('#btn-buscar-cep').find('span').addClass('d-none');
-            })
 
     })
 })
